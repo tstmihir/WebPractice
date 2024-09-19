a862d1b894f28e2eb8add1c37a84f6c33b8a5b3e
tstmihir <mihir.tst@gmail.com>
2024-09-19 18:51:35 +0530

    Project-5 progress

--- JS/project1/script.js
+++ JS/project1/script.js
@@ -1,12 +1,12 @@
 "use strict";
 
-let score = 20;
+let score = 20.5545;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let highscore = 0;
 
-const displayMessage = function(message){
-  document.querySelector('.message').textContent = message;
-}
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -19,20 +19,22 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
     document.querySelector(".number").textContent = secretNumber;
-    if(score > highscore){
+    if (score > highscore) {
       highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
+      document.querySelector(".highscore").textContent = highscore;
     }
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      displayMessage(guess > secretNumber ? "📈 Number is high": "📉 Number is low");
+      displayMessage(
+        guess > secretNumber ? "📈 Number is high" : "📉 Number is low"
+      );
       score--;
       document.querySelector(".score").textContent = score;
     } else {
       displayMessage("You Lose Game Over! 💥");
       document.querySelector(".score").textContent = 0;
     }
-  } 
+  }
 });
 document.querySelector(".again").addEventListener("click", function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
