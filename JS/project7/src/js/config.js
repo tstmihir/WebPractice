7d7a24a43c00f2331a6bda913a04cf295ac46f33
tstmihir <mihir.tst@gmail.com>
2024-09-30 16:36:33 +0530

    Remove node_modules from Git tracking

---
+++ JS/project7/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "464792ca-f527-470a-859b-84f8360c53db";
+export const MODAL_CLOSE_SEC = 2.5;
